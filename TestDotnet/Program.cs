62d01ed54f4d31ec345c007d7e24f1179ba0f1ff
Snowbeast138 <snowbeast138@gmail.com>
2024-02-07 18:07:00 -0600

    Add Categorys

--- TestDotnet/Program.cs
+++ TestDotnet/Program.cs
@@ -6,16 +6,17 @@ class Player
 {
     private string name;
 
-    private int health = 100;
+    private float health = 100;
 
-    private int damage = 5;
+    private float damage = 5;
 
     private bool gender = true;
 
-    private int speed = 10;
+    private float speed = 10;
 
     public void setGender()
     {
+        WriteLine("Elige tu Genero:\nTrue=Hombre\nFalse-Mujer");
         bool flag = bool.TryParse(ReadLine(), out gender);
         do
         {
@@ -23,20 +24,22 @@ class Player
             flag = bool.TryParse(ReadLine(), out gender);
         }
         while (!flag);
-        
+
         if (gender)
         {
-            WriteLine("AHHH Un Tioooo!!!");
+            WriteLine("Okay :D eres un chico eh");
         }
         else
         {
-            WriteLine("Seguro que eres una chica ? ,':B");
+            WriteLine("Okay eres una chica eh? :D");
         }
     }
 
     public void setName()
     {
+        WriteLine("Introduce tu Nombre");
         name = ReadLine();
+        WriteLine($"Bienvenido {name}");
     }
 
     public void Walk()
@@ -50,18 +53,119 @@ class Player
         damage = 5;
         speed = 10;
     }
+
+    public void incressStats(float boost)
+    {
+        health *= boost;
+        damage *= boost;
+        speed *= boost;
+    }
+
+    public void showStats()
+    {
+        WriteLine($"|||  {name}  |||\nVIDA:{health}     DAÑO:{damage}   VELOCIDAD:{speed}");
+        if(gender){
+            WriteLine("Sexo:Hombre");
+        }else{
+            WriteLine("Sexo:Mujer");
+        }
+    }
+}
+
+class Wizard : Player
+{
+    public float boost = 1.1F;
+    public void Fireball()
+    {
+        WriteLine($"Haz Lanzado una Bola de Fuego");
+    }
+}
+
+class Knight : Player
+{
+    public float boost = 2.5F;
+
+    public void Thrust()
+    {
+        WriteLine("Ha Dado una estocada");
+    }
+}
+
+class Paladin : Player
+{
+    public float boost = 1.5F;
+    public void Heal()
+    {
+        WriteLine("Se ah Curado");
+    }
+}
+
+class Hunter : Player
+{
+    public float boost = 2F;
+
+    public void AxeAttack()
+    {
+        WriteLine("A metido un Hachazo");
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Player player = new Player();
-        player.setGender();
+        ChooiseFacction();
     }
 
-    void ChooiseFacction(Player player)
+    public static void ChooiseFacction()
     {
-        WriteLine($"Selecciona tu Clase: 1-Wizard\n2-Knight\n3-Paladin\n4-Hunter\n");
+        int res;
+        bool flag;
+        do
+        {
+            WriteLine($"Selecciona tu Clase:\n1-Wizard\n2-Knight\n3-Paladin\n4-Hunter\n");
+            flag = int.TryParse(ReadLine(), out res);
+            if (res < 1 || res > 4)
+            {
+                flag = false;
+            }
+        }
+        while (!flag);
+
+        switch (res)
+        {
+            case 1:{
+                Wizard wizard = new();
+                wizard.setGender();
+                wizard.setName();
+                wizard.incressStats(wizard.boost);
+                wizard.showStats();
+            }break;
+            case 2:{
+                Knight knight = new();
+                knight.setGender();
+                knight.setName();
+                knight.incressStats(knight.boost);
+                knight.showStats();
+            }break;
+            case 3:{
+                Paladin paladin = new();
+                paladin.setGender();
+                paladin.setName();
+                paladin.incressStats(paladin.boost);
+                paladin.showStats();
+            }break;
+            case 4:{
+                Hunter hunter = new();
+                hunter.setGender();
+                hunter.setName();
+                hunter.incressStats(hunter.boost);
+                hunter.showStats();
+            }break;
+            default:
+                {
+                    WriteLine("");
+                }break;
+        }
     }
 }
